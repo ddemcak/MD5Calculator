2a414c2468f4193b62492e918c9152f6eed2369c
David Demcak <ddemcak@gmail.com>
2021-10-22 23:35:59 +0200

    Initial commit.

---
+++ MD5Calculator.ps1
@@ -0,0 +1,47 @@
+Write-Host '------------------------------' -ForegroundColor Yellow
+Write-Host '        MD5 Calculator        ' -ForegroundColor Yellow
+Write-Host '------------------------------' -ForegroundColor Yellow
+
+# Check if a 1 paramter1 were given.
+If ($args.Count -ne 1)
+{
+    #Write-Host 'Please provide hash filename!' -ForegroundColor Red
+    $hashfile = Read-Host -Prompt "Please provide hash filename"
+    
+}
+Else
+{
+    # MD5 filename
+    $hashfile = $args[0]
+}
+
+# Inform user about the filepath.
+Write-Host "MD5 filename: $hashfile"
+
+If (Test-Path -Path $hashfile -PathType leaf)
+{
+    Remove-Item $hashfile
+    Write-Host "$hashfile was deleted, beause it's not needed anymore." -ForegroundColor Red
+}
+
+# Get the path without the filename.
+$directory = $hashfile | Split-Path
+
+# We will collect all hashes and filenames here.
+$fileContent = ""
+
+# Go via all files in the directory and calculate MD5, add two spaces and filename.
+# Concatane results into the sum string.
+Get-ChildItem $directory | 
+Foreach-Object {
+    $md5 = Get-FileHash $_.FullName -Algorithm MD5
+
+    $filehash = $md5.Hash.ToLower()
+    $filename = $_
+
+    $fileContent += "$filehash  $filename`n" 
+}
+
+# Print MD5 hashes to file.
+$fileContent | Out-File -FilePath $hashfile -NoNewline
+Write-Host "$hashfile was created." -ForegroundColor Green
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# MD5Calculator
+# MD5 Calculator
 Small script capable of calculating MD5 hash of all files within the directory
+
+# Parameters
+Only one parameter is required with provided filepath of file that will contains all calculated MD5s within the same directory.
+
+### Description
+The sript creates a file that contains list of files with calculates MD5 hashes.
