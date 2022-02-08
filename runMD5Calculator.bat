1cae9606f95824155665c7d1adc8fd046c50c187
David Demcak <ddemcak@gmail.com>
2022-02-08 23:28:52 +0100

    Batch file was simplfied and works as expected - No file provided it askes for one otherwise it uses the provided arg as MD5 filepath.

--- runMD5Calculator.bat
+++ runMD5Calculator.bat
@@ -1,2 +1,2 @@
-rem powershell -ExecutionPolicy ByPass -File "MD5Calculator.ps1" "c:\Temp\Files\checksum.md5"
-powershell -ExecutionPolicy ByPass -File "MD5Calculator.ps1"
+powershell -ExecutionPolicy ByPass -File "MD5Calculator.ps1" %1
+pause
